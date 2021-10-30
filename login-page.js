ca15be70aeb1e1c636e83299c85ad8410fa76315
MineDudeYT <71100622+MineDudeYT@users.noreply.github.com>
2021-10-30 18:55:12 -0400

    Update login-page.js

--- login-page.js
+++ login-page.js
@@ -2,9 +2,6 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
-var username_github = ${{ secrets.USERNAME }};
-var password_github = ${{ secrets.PASSWORD }};
-
 // When the login button is clicked, the following code is executed
 loginButton.addEventListener("click", (e) => {
     // Prevent the default submission of the form
@@ -12,6 +9,8 @@ loginButton.addEventListener("click", (e) => {
     // Get the values input by the user in the form fields
     const username = loginForm.username.value;
     const password = loginForm.password.value;
+    const username_github = ${{ secrets.USERNAME }};
+    const password_github = ${{ secrets.PASSWORD }};
 
     if (username === username_github && password === password_github) {
         // If the credentials are valid, show an alert box and reload the page
