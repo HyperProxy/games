63f840261d93ea7d16c0d1d384d0d00c72504816
MineDudeYT <71100622+MineDudeYT@users.noreply.github.com>
2021-10-30 18:46:18 -0400

    Update login-page.js

--- login-page.js
+++ login-page.js
@@ -10,7 +10,7 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "gaming1234" && password === "hehethatsfunny") {
+    if (username === "${{ secrets.username }}" && password === "${{ secrets.password }}") {
         // If the credentials are valid, show an alert box and reload the page
         alert("You have successfully logged in.");
         window.location.replace('games.html');
